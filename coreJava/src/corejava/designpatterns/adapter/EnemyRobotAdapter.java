810382d567ba3520392edbbcdcd1d60ef405fe30
unknown <gshaik@CC002149.corp.SE.sempra.com>
2016-04-02 23:09:19 -0700

    adapter

---
+++ coreJava/src/corejava/designpatterns/adapter/EnemyRobotAdapter.java
@@ -0,0 +1,24 @@
+package corejava.designpatterns.adapter;
+
+
+public class EnemyRobotAdapter implements EnemyAttacker {
+
+    EnemyRobot theRobot;
+
+    public EnemyRobotAdapter(EnemyRobot newRobot) {
+        theRobot = newRobot;
+    }
+
+    public void fireWeapon() {
+        theRobot.smashWithHands();
+    }
+
+    public void driveForward() {
+        theRobot.walkForward();
+    }
+
+    public void assignDriver(String driverName) {
+
+        theRobot.reactToHuman(driverName);
+    }
+}
